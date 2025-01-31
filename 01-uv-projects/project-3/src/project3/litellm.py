57382d05663a70569eb4b1d6c986e12fb66513e4
Muneer Iqbal <128808006+MSMuneerIqbal@users.noreply.github.com>
2025-02-01 00:26:24 +0500

    Update litellm.py

--- 01-uv-projects/project-3/src/project3/litellm.py
+++ 01-uv-projects/project-3/src/project3/litellm.py
@@ -3,7 +3,7 @@ from litellm import completion
 import os
 
 ## set ENV variables
-os.environ["GEMINI_API_KEY"] = "AIzaSyAKbche_LIP23hXp1YCjXHFXNCBZEGRHy0"
+os.environ["GEMINI_API_KEY"] = "gemini api key"
 def call_gemini():
   response = completion(
   model="gemini/gemini-1.5-flash",
